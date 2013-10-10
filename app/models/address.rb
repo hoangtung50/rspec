6d816afe7ca0c351ffe98ff769adf18e51cf9110
HoangTung <hoangtung@hoangtung-K43E.(none)>
2013-10-10 15:26:53 +0700

    Modified address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ActiveRecord::Base
+  belongs_to :user
+  validates_presence_of :street, :city, :state, :country_code
+  validates :country_code, format: { with: /\A[a-zA-Z]{2}/}
+  before_save do
+    self.country_code = 'US' if self.country_code.blank?
+  end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Address do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'must have a street' do
+    a = Address.new
+    a.should_not be_valid
+    a.errors[:street].should_not be_nil
+  end
+  it 'must have a city' do
+    a = Address.new
+    a.should_not be_valid
+    a.errors[:city].should_not be_nil
+  end
+  it 'must have a state' do
+    a = Address.new
+    a.should_not be_valid
+    a.errors[:state].should_not be_nil
+  end
 end
